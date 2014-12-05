e90538d3c753745bb823dcb5bd59a4ea598d80fd
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-12-05 15:15:30 +0100

    Ellipsis of depth 2 works. That is x ... ... works.

--- fancier-quote.rkt
+++ fancier-quote.rkt
@@ -15,30 +15,26 @@
   (define (displayln* x) (displayln x) x)
   (define pattern-variables (make-free-id-table))
   (define (var? x) (free-id-table-ref pattern-variables x #f))
-  (define-syntax-class dots (pattern (~literal ...)))
+  (define-syntax-class dots     (pattern       (~literal ...)))
+  (define-syntax-class non-dots (pattern (~not (~literal ...))))
   (define msg1 "no pattern variables found - expected due to ellipsis")
   (define msg2 
     "pattern variables at the same ellipsis depth must be bound to lists of the same length")
   (define (qq0 stx)
     (syntax-parse stx
       [()              (syntax/loc stx '())]
-      [(s d:dots . sn) (define-values (expr vars) (qq1 #'s))
-                       (when (null? vars) (raise-syntax-error 'qq0 msg1  #'s))
-                       (with-lifts (vars expr msg2) 
-                         ([exprn (qq0 #'sn)])
-                         (syntax/loc stx 
-                           (let ()
-                             (define xss (list . vars))
-                             (unless (null? (cdr xss))
-                               (unless (apply = (map (λ(xs) (and (list? xs) (length xs))) xss))
-                                 (raise-syntax-error 'qq msg2 #'s)))
-                             (append (map (λ vars expr) . vars)
-                                     exprn))))]
-      [(s d:dots)      (define-values (expr vars) (qq1 #'s))
-                       (when (null? vars) (raise-syntax-error 'qq0 msg1  #'s))
-                       (with-syntax ([vars vars] [expr expr])
-                         (syntax/loc stx (map (λ vars expr) . vars)))]
-      
+      [(s d:dots ds:dots ... sn:non-dots ...)
+       (define sns #'(sn ...))
+       (displayln (list 'qq0-dots #'s #'d #'(ds ...) sns))
+       (define-values (expr vars) (qq1 #'(s ds ...)))
+       (when (null? vars) (raise-syntax-error 'qq0 msg1  #'s))
+       (with-lifts (vars expr msg2) ([exprn (qq0 #'(sn ...))])
+         (syntax/loc stx 
+           (let ([xss (list . vars)])
+             (unless (null? (cdr xss))
+               (unless (apply = (map (λ(xs) (and (list? xs) (length xs))) xss))
+                 (raise-syntax-error 'qq msg2 #'s)))
+             (append (append-map (λ vars expr) . vars)  exprn))))]
       [(a . d)    (quasisyntax/loc stx (cons #,(qq0 #'a) #,(qq0 #'d)))]
       [x:id       (if (var? #'x)
                       (syntax/loc stx x)
@@ -46,6 +42,34 @@
       [_          stx]))
   (define (qq1 stx)
     (displayln (list 'qq1-in stx))
+    (define seen (make-free-id-table))
+    (define (seen! x) (displayln (list 'saw: x)) (free-id-table-set! seen x #t))
+    (define (vars-seen) (free-id-table-map seen (λ (x _) x)))
+    (define (r stx)
+      (syntax-parse stx
+        [()      (syntax/loc stx '())]
+        [(s d:dots ds:dots ... sn:non-dots ...)
+         (define sns #'(sn ...))
+         (displayln (list 'qq1-dots #'s #'d #'(ds ...) sns))
+         (define-values (expr vars) (qq2 #'(s ds ...)))
+         (for-each seen! vars)
+         (when (null? vars) (raise-syntax-error 'qq1 msg1  #'s))
+         (define-values (sn-expr sn-vars) (qq1 #'(sn ...)))
+         (with-lifts (vars expr msg2) ([exprn sn-expr])
+           (syntax/loc stx 
+             (let ([xss (list . vars)])
+               (unless (null? (cdr xss))
+                 (unless (apply = (map (λ(xs) (and (list? xs) (length xs))) xss))
+                   (raise-syntax-error 'qq msg2 #'s)))
+               (append (append-map (λ vars expr) . vars)  exprn))))]
+        [(a . d) (quasisyntax/loc stx (cons #,(r #'a) #,(r #'d)))]
+        [x:id    (if (var? #'x) 
+                     (begin (seen! #'x) #'x)
+                     (syntax/loc stx 'x))]
+        [_       stx]))
+    (values (r stx) (displayln* (vars-seen))))
+  (define (qq2 stx)
+    (displayln (list 'qq2-in stx))
     (define seen (make-free-id-table))
     (define (seen! x) (displayln (list 'saw: x)) (free-id-table-set! seen x #t))
     (define (vars-seen) (free-id-table-map seen (λ (x _) x)))
@@ -74,5 +98,6 @@
                 '(           1         x         (1 x)         (1 x (2 x))))
   (check-equal? (let ([x '(x1 x2)]) (qq (x) (x ...))) '(x1 x2))
   (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x)   ((x 1 y) ...)))  '((x1 1 y)  (x2 1 y)))
-  (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x y) ((x 1 y1) ...))) '((x1 1 y1) (x2 1 y1))))
+  (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x y) ((x 1 y1) ...))) '((x1 1 y1) (x2 1 y1)))
+  (check-equal? (let ([x '((x1) (x2 x3) (x4 x5 x6))]) (qq (x y) (x ... ...))) '(x1 x2 x3 x4 x5 x6)))
 
