98d080867a7fdd15b1c97fbdde26b389861484e6
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-12-07 19:34:20 +0100

    Arbitrary depths work

--- fancier-quote.rkt
+++ fancier-quote.rkt
@@ -20,76 +20,94 @@
   (define msg1 "no pattern variables found - expected due to ellipsis")
   (define msg2 
     "pattern variables at the same ellipsis depth must be bound to lists of the same length")
-  (define (qq0 stx)
+  (define msg3 "... may appear only in proper lists")
+  (define msg4 "... must follow an s-expression")
+  (define (qq0 tvars stx)
     (syntax-parse stx
-      [()              (syntax/loc stx '())]
-      [(s d:dots ds:dots ... sn:non-dots ...)
-       (define sns #'(sn ...))
-       (displayln (list 'qq0-dots #'s #'d #'(ds ...) sns))
-       (define-values (expr vars) (qq1 #'(s ds ...)))
-       (when (null? vars) (raise-syntax-error 'qq0 msg1  #'s))
-       (with-lifts (vars expr msg2) ([exprn (qq0 #'(sn ...))])
+      [() (syntax/loc stx '())]
+      ; lists with at least one ellipsis
+      [(more ... s:non-dots d:dots ds:dots ... sn:non-dots ...)
+       ; The more ... is recursively expanded
+       (define more-expr (with-lifts (tvars) () 
+                           #'(qq tvars (more ...))))
+       ; s is expanded at a level higher (and omit one ...)
+       (define-values (s-expr s-vars) (qq1 tvars 1 #'(s ds ...)))
+       ; (displayln (list #'(s ds ...) s-vars))
+       ; at least one variable must be present in s
+       (when (null? s-vars) (raise-syntax-error 'qq0 msg1 #'s))
+       ; at the end we have sn ... which is a the same level
+       (define sn-expr (qq0 #'tvars #'(sn ...)))
+       ; the result is: (list* more ... s-expr sn-expr)
+       (with-lifts (more-expr s-expr sn-expr s-vars msg2) ()
          (syntax/loc stx 
-           (let ([xss (list . vars)])
+           (let ([xss (list . s-vars)])
+             ; check that variables at the same level are bound
+             ; to lists of the same lengths
              (unless (null? (cdr xss))
                (unless (apply = (map (λ(xs) (and (list? xs) (length xs))) xss))
                  (raise-syntax-error 'qq msg2 #'s)))
-             (append (append-map (λ vars expr) . vars)  exprn))))]
-      [(a . d)    (quasisyntax/loc stx (cons #,(qq0 #'a) #,(qq0 #'d)))]
-      [x:id       (if (var? #'x)
-                      (syntax/loc stx x)
-                      (syntax/loc stx 'x))]
-      [_          stx]))
-  (define (qq1 stx)
-    (displayln (list 'qq1-in stx))
+             ; construct the list from the pieces, mapping over s-vars for s
+             (append more-expr
+                     (append-map (λ s-vars s-expr) . s-vars)
+                     sn-expr))))]
+      [(_ . d:dots) (raise-syntax-error 'qq msg3 stx)]
+      [(a:dots . _) (raise-syntax-error 'qq msg4 stx)]
+      [(a . d)      (quasisyntax/loc stx (cons #,(qq0 tvars #'a) #,(qq0 tvars #'d)))]
+      ; identifiers and template variables
+      [x:id         (if (var? #'x)
+                        (syntax/loc stx x)
+                        (syntax/loc stx 'x))]
+      [_  stx]))
+  (define (qq1 tvars level stx)
+    ; keep a table over template variables used in stx,
+    ; the maximum depth of each template variable can be used for early error checking
     (define seen (make-free-id-table))
-    (define (seen! x) (displayln (list 'saw: x)) (free-id-table-set! seen x #t))
+    (define (seen! x) (free-id-table-set! seen x (max level (free-id-table-ref seen x (λ () level)))))
     (define (vars-seen) (free-id-table-map seen (λ (x _) x)))
     (define (r stx)
       (syntax-parse stx
-        [()      (syntax/loc stx '())]
-        [(s d:dots ds:dots ... sn:non-dots ...)
-         (define sns #'(sn ...))
-         (displayln (list 'qq1-dots #'s #'d #'(ds ...) sns))
-         (define-values (expr vars) (qq2 #'(s ds ...)))
-         (for-each seen! vars)
-         (when (null? vars) (raise-syntax-error 'qq1 msg1  #'s))
-         (define-values (sn-expr sn-vars) (qq1 #'(sn ...)))
-         (with-lifts (vars expr msg2) ([exprn sn-expr])
+        [() (syntax/loc stx '())]
+        ; lists with at least one ellipsis
+        [(more ... s:non-dots d:dots ds:dots ... sn:non-dots ...)
+         ; The more ... is recursively expanded
+         (define-values (more-expr more-vars) (qq1 tvars level #'(more ...)))
+         (for-each seen! more-vars)
+         ; s is expanded at a level higher (and omit one ...)
+         (define-values (s-expr s-vars) (qq1 tvars (+ level 1) #'(s ds ...)))
+         (for-each seen! s-vars)
+         ; at least one variable must be present in s
+         (when (null? s-vars) (raise-syntax-error 'qq0 msg1 #'s))
+         ; at the end we have sn ... which is a the same level
+         (define-values (sn-expr sn-vars) (qq1 tvars level #'(sn ...)))
+         (for-each seen! sn-vars)
+         ; the result is: (list* more ... s-expr sn-expr)
+         (with-lifts (more-expr s-expr sn-expr s-vars msg2) ()
            (syntax/loc stx 
-             (let ([xss (list . vars)])
+             (let ([xss (list . s-vars)])
+               ; check that variables at the same level are bound
+               ; to lists of the same lengths
                (unless (null? (cdr xss))
                  (unless (apply = (map (λ(xs) (and (list? xs) (length xs))) xss))
                    (raise-syntax-error 'qq msg2 #'s)))
-               (append (append-map (λ vars expr) . vars)  exprn))))]
-        [(a . d) (quasisyntax/loc stx (cons #,(r #'a) #,(r #'d)))]
-        [x:id    (if (var? #'x) 
-                     (begin (seen! #'x) #'x)
-                     (syntax/loc stx 'x))]
-        [_       stx]))
-    (values (r stx) (displayln* (vars-seen))))
-  (define (qq2 stx)
-    (displayln (list 'qq2-in stx))
-    (define seen (make-free-id-table))
-    (define (seen! x) (displayln (list 'saw: x)) (free-id-table-set! seen x #t))
-    (define (vars-seen) (free-id-table-map seen (λ (x _) x)))
-    (define (r stx)
-      (syntax-parse stx
-        [()      (syntax/loc stx '())]
-        [(a . d) (quasisyntax/loc stx (cons #,(r #'a) #,(r #'d)))]
-        [x:id    (if (var? #'x) 
-                     (begin (seen! #'x) #'x)
-                     (syntax/loc stx 'x))]
-        [_       stx]))
-    (values (r stx) (displayln* (vars-seen))))
-  
+               ; construct the list from the pieces, mapping over s-vars for s
+               (append more-expr
+                       (append-map (λ s-vars s-expr) . s-vars)
+                       sn-expr))))]
+        [(_ . d:dots) (raise-syntax-error 'qq msg3 stx)]
+        [(a:dots . _) (raise-syntax-error 'qq msg4 stx)]
+        [(a . d)      (quasisyntax/loc stx (cons #,(r #'a) #,(r #'d)))]
+        [x:id         (if (var? #'x) 
+                          (begin (seen! #'x) #'x)
+                          (syntax/loc stx 'x))]
+        [_            stx]))
+    (define r-stx (r stx))
+    (values r-stx (vars-seen)))
   (syntax-parse stx
     [(_ (~and vars (var ...)) s-exp)
      (for ([v (in-list (syntax->list #'vars))])
        (free-id-table-set! pattern-variables v #t))
-     (qq0 #'s-exp)]))
-
-(expand #'(qq () (1 x)))
+     ; (define-values (expr vars) (qq1 #'vars 0 #'s-exp)) expr
+     (qq0 #'vars #'s-exp)]))
 
 (define x 42)
 
@@ -99,5 +117,9 @@
   (check-equal? (let ([x '(x1 x2)]) (qq (x) (x ...))) '(x1 x2))
   (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x)   ((x 1 y) ...)))  '((x1 1 y)  (x2 1 y)))
   (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x y) ((x 1 y1) ...))) '((x1 1 y1) (x2 1 y1)))
-  (check-equal? (let ([x '((x1) (x2 x3) (x4 x5 x6))]) (qq (x y) (x ... ...))) '(x1 x2 x3 x4 x5 x6)))
+  (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x y) (x ... y ...))) '(x1 x2 y1 y2))
+  (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x y) (1 x ... 2 y ...))) '(1 x1 x2 2 y1 y2))
+  #;(check-equal? (let ([x '((x1) (x2 x3) (x4 x5 x6))]) (qq (x y) (x ... ...))) '(x1 x2 x3 x4 x5 x6)))
 
+#;(let ([x '((x1) (x2 x3) (x4 x5 x6))] [y '(a b c)])
+    (qq (x y) ((x ... ... y ...))))
