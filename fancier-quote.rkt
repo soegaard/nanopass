52541b0d70be6197066b1be129ce26a32fd638e8
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-12-07 20:37:31 +0100

    Fixed bug related to nested ellipsis. Better error messages.

--- fancier-quote.rkt
+++ fancier-quote.rkt
@@ -31,7 +31,9 @@
        (define more-expr (with-lifts (tvars) () 
                            #'(qq tvars (more ...))))
        ; s is expanded at a level higher (and omit one ...)
-       (define-values (s-expr s-vars) (qq1 tvars 1 #'(s ds ...)))
+       (define levels (make-free-id-table))
+       (define-values (s-expr s-vars) (qq1 levels tvars 1 #'(s ds ...)))
+       ; (displayln (list 'levels (free-id-table-map levels list)))
        ; (displayln (list #'(s ds ...) s-vars))
        ; at least one variable must be present in s
        (when (null? s-vars) (raise-syntax-error 'qq0 msg1 #'s))
@@ -45,7 +47,7 @@
              ; to lists of the same lengths
              (unless (null? (cdr xss))
                (unless (apply = (map (λ(xs) (and (list? xs) (length xs))) xss))
-                 (raise-syntax-error 'qq msg2 #'s)))
+                 (raise-syntax-error 'qq msg2 #'s-expr)))
              ; construct the list from the pieces, mapping over s-vars for s
              (append more-expr
                      (append-map (λ s-vars s-expr) . s-vars)
@@ -54,15 +56,19 @@
       [(a:dots . _) (raise-syntax-error 'qq msg4 stx)]
       [(a . d)      (quasisyntax/loc stx (cons #,(qq0 tvars #'a) #,(qq0 tvars #'d)))]
       ; identifiers and template variables
+      [a:dots       (raise-syntax-error 'qq "bad syntax" #'a)]
       [x:id         (if (var? #'x)
                         (syntax/loc stx x)
                         (syntax/loc stx 'x))]
       [_  stx]))
-  (define (qq1 tvars level stx)
+  (define (qq1 levels tvars level stx)
+    ; (displayln (list 'qq1 levels tvars level stx))
     ; keep a table over template variables used in stx,
     ; the maximum depth of each template variable can be used for early error checking
     (define seen (make-free-id-table))
     (define (seen! x) (free-id-table-set! seen x (max level (free-id-table-ref seen x (λ () level)))))
+    (define (level! x) 
+      (free-id-table-set! levels x (max level (free-id-table-ref levels x (λ () level)))))
     (define (vars-seen) (free-id-table-map seen (λ (x _) x)))
     (define (r stx)
       (syntax-parse stx
@@ -70,16 +76,19 @@
         ; lists with at least one ellipsis
         [(more ... s:non-dots d:dots ds:dots ... sn:non-dots ...)
          ; The more ... is recursively expanded
-         (define-values (more-expr more-vars) (qq1 tvars level #'(more ...)))
-         (for-each seen! more-vars)
+         (define-values (more-expr more-vars) (qq1 levels tvars level #'(more ...)))
+         (for-each seen!  more-vars)
+         (for-each level! more-vars)
          ; s is expanded at a level higher (and omit one ...)
-         (define-values (s-expr s-vars) (qq1 tvars (+ level 1) #'(s ds ...)))
-         (for-each seen! s-vars)
+         (define-values (s-expr s-vars) (qq1 levels tvars (+ level 1) #'(s ds ...)))
+         (for-each seen!  s-vars)
+         (for-each level! s-vars)
          ; at least one variable must be present in s
          (when (null? s-vars) (raise-syntax-error 'qq0 msg1 #'s))
          ; at the end we have sn ... which is a the same level
-         (define-values (sn-expr sn-vars) (qq1 tvars level #'(sn ...)))
-         (for-each seen! sn-vars)
+         (define-values (sn-expr sn-vars) (qq1 levels tvars level #'(sn ...)))
+         (for-each seen!  sn-vars)
+         (for-each level! sn-vars)
          ; the result is: (list* more ... s-expr sn-expr)
          (with-lifts (more-expr s-expr sn-expr s-vars msg2) ()
            (syntax/loc stx 
@@ -88,7 +97,7 @@
                ; to lists of the same lengths
                (unless (null? (cdr xss))
                  (unless (apply = (map (λ(xs) (and (list? xs) (length xs))) xss))
-                   (raise-syntax-error 'qq msg2 #'s)))
+                   (raise-syntax-error 'qq msg2 #'s-expr)))
                ; construct the list from the pieces, mapping over s-vars for s
                (append more-expr
                        (append-map (λ s-vars s-expr) . s-vars)
@@ -96,6 +105,7 @@
         [(_ . d:dots) (raise-syntax-error 'qq msg3 stx)]
         [(a:dots . _) (raise-syntax-error 'qq msg4 stx)]
         [(a . d)      (quasisyntax/loc stx (cons #,(r #'a) #,(r #'d)))]
+        [a:dots       (error)]
         [x:id         (if (var? #'x) 
                           (begin (seen! #'x) #'x)
                           (syntax/loc stx 'x))]
@@ -107,9 +117,11 @@
      (for ([v (in-list (syntax->list #'vars))])
        (free-id-table-set! pattern-variables v #t))
      ; (define-values (expr vars) (qq1 #'vars 0 #'s-exp)) expr
-     (qq0 #'vars #'s-exp)]))
-
-(define x 42)
+     (qq0 #'vars #'s-exp)]
+    [(_ vars s-exp more0 more1 ...)
+     (raise-syntax-error 
+      'qq "expected (qq (<template-variable> ...) s-expression), got an extra s-expression" #'more0)]
+    [_ (raise-syntax-error 'qq "expected (qq (<template-variable> ...) s-expression), got Y" stx)]))
 
 (module+ test (require rackunit)
   (check-equal? (list (qq () 1) (qq () x) (qq () (1 x)) (qq () (1 x (2 x))))
@@ -119,7 +131,14 @@
   (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x y) ((x 1 y1) ...))) '((x1 1 y1) (x2 1 y1)))
   (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x y) (x ... y ...))) '(x1 x2 y1 y2))
   (check-equal? (let ([x '(x1 x2)] [y '(y1 y2)]) (qq (x y) (1 x ... 2 y ...))) '(1 x1 x2 2 y1 y2))
-  #;(check-equal? (let ([x '((x1) (x2 x3) (x4 x5 x6))]) (qq (x y) (x ... ...))) '(x1 x2 x3 x4 x5 x6)))
+  (check-equal? (let ([x '((x1) (x2 x3) (x4 x5 x6))]) (qq (x y) (x ... ...))) '(x1 x2 x3 x4 x5 x6))
+  (check-equal? (let ([x '((x1) (x2 x3) (x4 x5 x6))] [y '(a b c)])
+                  (qq (x y) (x ... ... y ...)))
+                '(x1 x2 x3 x4 x5 x6 a b c))
+  (check-equal? (let ([x '((x1 x2) (x3 x4 x5))] [y '(y1 y2)]) 
+                  (qq (x y) ((x ...) ... y ...)))
+                '((x1 x2) (x3 x4 x5) y1 y2))
+  (check-equal? (let ([x '((x1 x2) (x3 x4 x5))] [y '(y1 y2)]) 
+                  (qq (x y) ((x ...) ...)))
+                '((x1 x2) (x3 x4 x5))))
 
-#;(let ([x '((x1) (x2 x3) (x4 x5 x6))] [y '(a b c)])
-    (qq (x y) ((x ... ... y ...))))
