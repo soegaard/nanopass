a55898043dd55d7a21a5fd60291c2191cdb6f460
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-12-16 16:19:37 +0100

    Added catas (still work to do on ellipsis variables)

--- construction.rkt
+++ construction.rkt
@@ -1,6 +1,8 @@
 #lang racket
 ;;; Notes: two level ... work (with explicit variables)
 
+;;; Current problem: catas are not working for ellipsis varibles
+
 (require (for-syntax syntax/parse
                      syntax/id-table)
          racket/stxparam)
@@ -9,21 +11,20 @@
 
 ; construct is to construction as match is to deconstruction
 
-; (construct 1)          => 1
+; (construct 1)             => 1
 ; For x unbound in the construct environment:
-; (construct x)          => 'x
-; (construct (x . d))    => (cons (construct x) (construct d))
+; (construct x)             => 'x
+; (construct (x . d))       => (cons (construct x) (construct d))
 ;; For x bound to construction transformer t in the construction environment
-; (construct x)          => (t x)
-; (construct (x a ...))  => (t x (list (construct a) ...)))
+; (construct x)             => (t x)
+; (construct (x a ...))     => (t x (list (construct a) ...)))
 ;; For x bound to v a non-transformer value in the construction environment
-; (construct x)          => x
-; (construct (x ...))    => ?
-
-
+; (construct x)             => x
+; (construct ((x ...) . d)  => (list* (construct x) ... d)
 
 (define-syntax-parameter env (make-immutable-free-id-table))
 (struct construction-transformer (transform))
+(struct construction-cata        (cata))
 
 (begin-for-syntax  
   (define (bound? id)   (free-id-table-ref (syntax-parameter-value #'env) id #f))
@@ -54,31 +55,41 @@
   (syntax-parse stx
     [(_ x:number)      #'x]
     [(_ x:unbound)     #''x]
-    [(_ x:bound)       #'(if (construction-transformer? x)
-                             ((construction-transformer-transform x))
-                             x)]
-    [(_ (x:bound . d)) #'(let ([vd (construct d)])
-                           (if (construction-transformer? x)
-                               (apply (construction-transformer-transform x) vd)
-                               (cons x vd)))]
+    [(_ x:bound)       #'(match x
+                           [(construction-transformer t) (t)]
+                           [(construction-cata c)        (c)]
+                           [_                             x])]
+    [(_ (x:bound d ...)) #'(let ([vds (list (construct d) ...)])
+                             (match x
+                               [(construction-transformer t) (apply t vds)]
+                               [(construction-cata        c) (cons (c) vds)]
+                               [_                            (cons x vds)]))]
+    [(_ (x:bound . d))  #'(let ([vd (construct d)])
+                            (match x
+                              ; [(construction-transformer t) (t vd)]
+                              [(construction-cata        c) (cons (c) vd)]
+                              [_                            (cons x vd)]))]
     [(_ (((~literal ...) {bv ...} ((~literal ...) {bv2 ...} a*)) . d))
      #'(append (append-map (λ (bv ...)
                              (with-constructors (bv ...)
                                (construct (((... ...) {bv2 ...} a*)))))
                            bv ...)
                (construct d))]
-    [(_ (((~literal ...) {bv ...} a*) . d))
-     (with-syntax (#;[(v ...) (bound-vars-of a*)])
-       #'(append (map (λ (bv ...)
-                        (with-constructors (bv ...)
-                          (construct a*)))
-                      bv ...)
-                 (construct d)))]
+    [(_ (((~literal ...) {bv ...} a*) . d)) (with-syntax (#;[(v ...) (bound-vars-of a*)])
+                                              #'(append (map (λ (bv ...)
+                                                               (with-constructors (bv ...)
+                                                                 (construct a*)))
+                                                             bv ...)
+                                                        (construct d)))]
+    [(_ (a . d))                         #'(cons (construct a) (construct d))]
+    [(_ ())                              #''()]
+    [(_ #:catas {c ...} f . more)        (with-syntax ([(t ...) (generate-temporaries #'(c ...))])
+                                           #'(let ([t (construction-cata (λ () (f c)))] ...)
+                                               (let ([c t] ...)
+                                                 (with-constructors (c ...)
+                                                   (construct . more)))))]
+    [(_ {v ...} . more)                  #'(with-constructors {v ...} (construct . more))]
     
-    
-    
-    [(_ (a . d))       #'(cons (construct a) (construct d))]
-    [(_ ())            #''()]
     [_ (error 'construct "got ~a" stx)]))
 
 
@@ -105,19 +116,71 @@
                 `(foo, (foo 'foo)))
   (check-equal? (let ([a '(1 2 3)])
                   (with-constructors (a)
-                    (construct (x (... {a} a) y))))      ; read as  x a ... y
+                    (construct (x (... {a} a) y))))            ; read as  x a ... y
                 '(x 1 2 3 y))
   (check-equal? (let ([a '(1 2 3)])
                   (with-constructors (a)
-                    (construct (x (... {a} (y a)) z))))  ; read as  x (y a) ... z
+                    (construct (x (... {a} (y a)) z))))        ; read as  x (y a) ... z
                 '(x (y 1) (y 2) (y 3) z))
-  (check-equal? (let ([a '((1 2 3) (x y z))])  ; <=== TODO 
+  (check-equal? (let ([a '((1 2 3) (x y z))])  
                   (with-constructors (a)
                     (construct (x (... {a} (... {a} a)) z))))  ; read as  x a ... ... y
                 '(x 1 2 3 x y z z))
-  #;(check-equal? (let ([a '((1 2 3) (x y z))] [b '(11 12)])  ; <=== TODO 
-                    (with-constructors (a)
-                      (construct (x (... {a b} (b (... {a} a))) z))))
-                  '(x (11 1 2 3) (12 x y z))))
+  (check-equal? (let ([a '((1 2 3) (x y z))] [b '(11 12)])
+                  (with-constructors (a)
+                    (construct (x (... {a b} (b (... {a} a))) z)))) ; reads as x (b a ...) ... z
+                '(x (11 1 2 3) (12 x y z) z)))
 
+(define (simplify-lite s-exp)
+  (define recur simplify-lite)
+  (displayln s-exp)
+  (match s-exp
+    ; datums
+    [(? symbol? x)                          x]
+    [(list)                                 '()]
+    [(? number? n)                          n]
+    ; 
+    [(list 'quote d)                        (construct {d} d)]
+    [(list 'if e0 e1)                       (construct #:catas {e0 e1} recur
+                                                       (if e0 e1 (void)))]
+    [(list 'if e0 e1 e2)                    (construct {e0 e1 e2}
+                                                       (if e0 e1 e2))]
+    [(list 'begin e* en)                    (construct #:catas {e* en} recur
+                                                       (begin e* en))]
+    [(list 'lambda (list x* ...) 
+           body* body)                      (construct {x*} #:catas {body* body} recur
+                                                       (lambda (x* ...) (begin body* body)))]
+    [(list 'letrec (list (list x* e*))
+           body*  body)                     (construct {x*} #:catas {e* body* body} recur
+                                                       (letrec ([x* e*]) (begin body* body)))]
+    [(list e e* ...)                        (construct #:catas {e e*} recur
+                                                       (e e* ...))]
+    [_ (error 'simplify-liet "got ~a" s-exp)]))
 
+(define (simplify s-exp)
+  (displayln s-exp)
+  (match s-exp
+    ; datums
+    [(? symbol? x)                          x]
+    [(list)                                 '()]
+    [(? number? n)                          n]
+    ; 
+    [(list 'quote d)                        (construct {d} d)]
+    [(list 'if e0 e1)                       (construct #:catas {e0 e1} simplify
+                                                       (if e0 e1 (void)))]
+    [(list 'if e0 e1 e2)                    (construct {e0 e1 e2}
+                                                       (if e0 e1 e2))]
+    [(list 'begin e* ... en)                (construct #:catas {e* en} simplify
+                                                       (begin e* ... en))]
+    [(list 'lambda (list x* ...) 
+           body* ... body)                  (construct {x*} #:catas {body* body} simplify
+                                                       (lambda (x* ...) (begin body* ... body)))]
+    [(list 'letrec (list (list x* e*) ...)
+           body* ... body)                  (construct {x*} #:catas {e* body* body} simplify
+                                                       (letrec ([x* e*] ...) (begin body* ... body)))]
+    [(list e e* ...)                        (construct #:catas {e e*} simplify
+                                                       (e e* ...))]
+    
+    [_ (error 'simplify "got ~a" s-exp)]))
+
+(simplify '(if 1 (letrec ([x (if 2 3)]) 4 5)))
