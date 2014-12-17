7056b301d09d1a27bd1e2964df767eba44e13ae8
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-12-17 17:59:01 +0100

    Cleaning - and adding start of a ... to (... a) rewriter

--- construction.rkt
+++ construction.rkt
@@ -1,44 +1,63 @@
 #lang racket
-;;; Current problem: catas are not working for ellipsis varibles
-
 (require (for-syntax syntax/parse
                      syntax/id-table)
          racket/stxparam)
 
 ;;; Construction
+;;;   construct is to construction as match is to deconstruction
+;;;   For datums and unbound identifiers, construct works like quote.
 
-; construct is to construction as match is to deconstruction
-
+;; Datums
 ; (construct 1)             => 1
-; For x unbound in the construction environment:
-; (construct x)             => 'x
-; (construct (x . d))       => (cons (construct x) (construct d))
-;; For x bound to construction transformer t in the construction environment
-; (construct x)             => (t x)
-; (construct (x a ...))     => (t x (list (construct a) ...)))
-;; For x bound to v a non-transformer value in the construction environment
-; (construct x)             => x
-; (construct ((x ...) . d)  => (list* (construct x) ... d)
 
+; For x unbound in the construction environment:
+;    (construct x)             => 'x
+;    (construct (x . d))       => (cons (construct x) (construct d))
+
+;; For x bound to construction transformer t in the construction environment
+;    (construct x)             => (t x)
+;    (construct (x a ...))     => (t x (list (construct a) ...)))
+
+;; For x bound to v a non-transformer value in the construction environment
+;    (construct x)             => x
+;    (construct ((x ...) . d)  => (list* (construct x) ... d)
+
+;;; During expansion a compile time environment is need to keep
+;;; track of which identifiers are bound as variables and catas.
 (define-syntax-parameter env    (make-immutable-free-id-table))
-(define-syntax-parameter levels (make-immutable-free-id-table))
-(define-syntax-parameter level  0)
+(begin-for-syntax (struct expansion-cata         (id) #:transparent))
 (struct construction-transformer (transform))
 (struct construction-cata        (cata))
-(begin-for-syntax
-  (struct expansion-cata         (id) #:transparent))
 
+; If (r x) = 'variable  
+; then x is a variable, which means x is evaluated at runtime.
+;    If x evaluates to (construction-transformer t), 
+;    then (construct (x v ...)) will insert evaluate (t v ...) and insert the result in the output.
+;    If x evaluates to a value v, then the value v is inserted in the output.
+; If (r x) = (expansion-cata f) 
+; then (f x) is inserted in the output.
+
+; syntax (with-variables (id ...) body ...)
+;    bind id ... as variables in the construction environment, then evaluate body ...
+;    Example:   > (let ([x 42]) (with-variables (x) (construct (1 x y))))
+;               '(1 42 y)
 (define-syntax (with-variables stx)
   (syntax-parse stx
-    [(_ (id ...) body ...)
+    [(_ (id:id ...) body ...)
      (define new-env
        (for/fold ([r (syntax-parameter-value #'env)]) ([id (syntax->list #'(id ...))])
          (if (free-id-table-ref r id #f)
              r ; don't override an existing binding
              (free-id-table-set r id 'variable))))
      #`(syntax-parameterize ([env #,new-env])
-                            body ...)]))
+                            body ...)]
+    [_ (raise-syntax-error 'with-variables "expected (with-variables (id ...) body ...)" stx)]))
 
+; syntax (with-catas (id ...) f body ...)
+;   Bind id ... as cata variables.
+;   If id is referenced inside a (construct ...), then (f id) is inserted.
+;   Example: > (let ([x 16]) (with-catas (x) sqrt (construct (9 x))))
+;            '(9 4)
 (define-syntax (with-catas stx)
   (syntax-parse stx
     [(_ (id:id ...) f body ...)
@@ -48,6 +67,8 @@
      #`(syntax-parameterize ([env #,new-env])
                             body ...)]))
 
+; syntax (with-catas* ([c f] ...) body ...)
+;   Bind the c ... as cata variables, the corresponding catamorphism is f.
 (define-syntax (with-catas* stx)
   (syntax-parse stx
     [(_ () body ...)                     #'(let () body ...)]
@@ -55,13 +76,15 @@
                                              (with-catas* ([c f] ...)
                                                body ...))]))
 
-(begin-for-syntax  
+(begin-for-syntax 
+  ;; References to variables in the construction environment
   (define (ref id) (free-id-table-ref (syntax-parameter-value #'env) id #f))
   (define (bound? id)    (ref id))
   (define (unbound? id)  (not (bound? id)))
   (define (variable? id) (eq? (ref id) 'variable))
   (define (cata? id) (expansion-cata? (ref id)))
   
+  ;; Syntax classes for the various variable types
   (define-syntax-class variable
     #:description "identifier bound in the construction environment"
     (pattern var:id
@@ -83,16 +106,88 @@
              #:attr f (cata? #'var))))
 
 
-(begin-for-syntax
-  (define flag #f)
-  (set! flag #f))
+;; Debug flag, if #t then construct will display useful info during expansion.
+(begin-for-syntax (define flag #f) (set! flag #f))
+
+(module reverse-syntax racket
+  (provide reverse-syntax)
+  (require racket/match)
+  (define (reverse-syntax stx)
+    (define loc (if (syntax? stx) stx #f))
+    (define r reverse-syntax)
+    (define as (if (syntax? stx) (syntax->list stx) stx))
+    (if (list? as) 
+        (datum->syntax loc (reverse (map r as)))
+        (let ()
+          (match (if (syntax? stx) (syntax-e stx) stx)
+            [(cons a d) (datum->syntax loc (cons (r d) (r a)))]
+            [_          stx])))))
+
+(require (for-syntax (submod "." reverse-syntax)))
+
+(module+ test
+  (require (submod ".." reverse-syntax))
+  (define sd syntax->datum)
+  (check-equal? (sd (reverse-syntax #'1)) 1)
+  (check-equal? (sd (reverse-syntax #'(1 2 3))) '(3 2 1))
+  (check-equal? (sd (reverse-syntax #'(3 . 4))) '(4 . 3))
+  (check-equal? (sd (reverse-syntax #'(3 . (4 . 5)))) '((5 . 4) . 3)))
+
+(module rewrite-ellipsis racket/base
+  (provide rewrite-ellipsis)
+  (require syntax/parse racket/list racket/match)
+  ; rewrite-ellipsis : syntax -> syntax
+  ;   input is in postfix form
+  ;   a ...      ->  (... a)
+  ;   a ... ...  ->  (... (... a))
+  (define (rewrite-ellipsis stx)
+    (displayln (list 'rw stx))
+    (define r rewrite-ellipsis)
+    (with-syntax ([dots #'(... ...)])
+      (define (dots? x) (and (identifier? x) (free-identifier=? x #'dots)))
+      (define (r stx)
+        (syntax-parse stx
+          [((~and a (~not (~literal ...))) . d)  
+           #`(a . #,(r #'d))]
+          [(a ...)
+           (define as (syntax->list #'(a ...)))
+           (define-values (ds a-more) (splitf-at as dots?))
+           (cond
+             ; no ellipsis, do nothing
+             [(null? ds) stx]
+             [else ; at least one ellipsis, wrap around the s-exp after 
+              (match a-more
+                [(cons a more) ; a is the s-exp to be repeated
+                 (define wrapped-a (for/fold ([a a]) ([d ds])
+                                     #`(#,a dots)))
+                 #`(#,wrapped-a . #,(r more))]
+                ['() (error 'rewrite-ellipis "no s-expression after ellipsis")])])]
+          [(a . d) #`(#,(r #'a) . #,(r #'d))]
+          [other   #'other]))
+      (r stx))))
+(require (submod "." rewrite-ellipsis))
+(module+ test 
+  (require (submod ".." rewrite-ellipsis))
+  (define (r stx) (reverse-syntax (rewrite-ellipsis (reverse-syntax stx))))
+  ; (define (r stx) (rewrite-ellipsis (reverse-syntax stx)))
+  (provide r)
+  (with-syntax ([ooo #'(... ...)])
+    (check-equal? (sd (r #'(1 2 3))) '(1 2 3))
+    (check-equal? (sd (r #'(1 x (... ...) 2))) '(1 (... x) 2))
+    (check-equal? (sd (r #'(1 x (... ...) (... ...) 2))) '(1 (... (... x)) 2))))
+
+  
+
+
+;; syntax (construct s-exp)
+;;   construct a value according to the s-exp template
 (define-syntax (construct stx)
-  (when flag (displayln stx))
-  (when flag (displayln (free-id-table-map (syntax-parameter-value #'env) list )))
-  (define l  (syntax-parameter-value #'level))
-  (define ls (syntax-parameter-value #'levels))
+  (when flag 
+    (displayln stx)
+    (displayln (free-id-table-map (syntax-parameter-value #'env) list)))
   (syntax-parse stx
     [(_ x:number)                      #'x]
+    ; [(_ (~literal ...))                (raise-syntax-error 'construct "illegal use of ... ~a" stx)]
     [(_ x:unbound)                     #''x]
     [(_ x:cata)                        (with-syntax ([f (expansion-cata-id (ref #'x))])
                                          #'(f x))]
@@ -100,7 +195,7 @@
                                            [(construction-transformer t) (t)]
                                            [(construction-cata c)        (c)]
                                            [_                             x])]
-    [(_ (x:cata     ((~literal ...) {bv ...} a*) . d)) ; <= TODO: find test case / is this case needed
+    [(_ (x:cata ((~literal ...) {bv ...} a*) . d)) ; <= TODO: find test case / is this case needed
      #'(cons (construct x) (construct (((... ...) {bv ...} a*) . d)))]
     [(_ (x:cata d ...))                #'(cons (construct x) (construct (d ...)))]
     [(_ (x:variable d ...))            #'(let ([vds (construct (d ...))])
@@ -118,11 +213,13 @@
                                      (construct (a*)))))
                                bv ...)
                    (construct d))))]
-    [(_ (a . d))                           #'(cons (construct a) (construct d))]
-    [(_ ())                                #''()]
-    [(_ #:catas {c ...} f . more)          #'(with-catas (c ...) f
-                                               (construct . more))]
+    ; [(_ ((~literal ...) . _))          (raise-syntax-error 'construct "illegal use of ..." stx)]
+    [(_ (a . d))                       #'(cons (construct a) (construct d))]
+    [(_ ())                            #''()]
+    [(_ #:catas {c ...} f . more)      #'(with-catas (c ...) f (construct . more))]
     [(_ {v ...} . more)                #'(with-variables {v ...} (construct . more))]
+    
+    [(_ datum)                         #'datum]
     [_ (displayln stx)
        (error 'construct "got ~a" stx)]))
 
@@ -185,32 +282,35 @@
                 '(1 2 3 4)))
 
 
-(define (simplify s-exp)
-  (displayln s-exp)
-  (match s-exp
-    ; datums
-    [(? symbol? x)                          x]
-    [(list)                                 '()]
-    [(? number? n)                          n]
-    ; 
-    [(list 'quote d)                        (construct {d} d)]
-    [(list 'if e0 e1)                       (construct #:catas {e0 e1} simplify
-                                                       (if e0 e1 (void)))]
-    [(list 'if e0 e1 e2)                    (construct {e0 e1 e2}
-                                                       (if e0 e1 e2))]
-    [(list 'begin e* ... en)                (construct #:catas {e* en} simplify
-                                                       (begin (... {e*} e*) en))]
-    [(list 'lambda (list x* ...) 
-           body* ... body)                  (construct {x*} #:catas {body* body} simplify
-                                                       (lambda ((... {x*} x*)) 
-                                                         (begin (... {body*} body*) body)))]
-    [(list 'letrec (list (list x* e*) ...)
-           body* ... body)                  (construct {x*} #:catas {e* body* body} simplify
-                                                       (letrec ((... {x* e*} [x* e*]))
-                                                         (begin (... {body*} body*) body)))]
-    [(list e e* ...)                        (construct #:catas {e e*} simplify
-                                                       (e e* ...))]
-    
-    [_ (error 'simplify "got ~a" s-exp)]))
+(module+ test
+  (define (simplify s-exp)
+    ; (displayln s-exp)
+    (match s-exp
+      [(? symbol? x)                          x]
+      [(list)                                 '()]
+      [(? number? n)                          n]
+      ; 
+      [(list 'quote d)                        (construct {d} d)]
+      [(list 'if e0 e1)                       (construct #:catas {e0 e1} simplify
+                                                         (if e0 e1 (void)))]
+      [(list 'if e0 e1 e2)                    (construct {e0 e1 e2}
+                                                         (if e0 e1 e2))]
+      [(list 'begin e* ... en)                (construct #:catas {e* en} simplify
+                                                         (begin (... {e*} e*) en))]
+      [(list 'lambda (list x* ...) 
+             body* ... body)                  (construct {x*} #:catas {body* body} simplify
+                                                         (lambda ((... {x*} x*)) 
+                                                           (begin (... {body*} body*) body)))]
+      [(list 'letrec (list (list x* e*) ...)
+             body* ... body)                  (construct {x*} #:catas {e* body* body} simplify
+                                                         (letrec ((... {x* e*} [x* e*]))
+                                                           (begin (... {body*} body*) body)))]
+      [(list e e* ...)                        (construct #:catas {e e*} simplify
+                                                         (e e* ...))]
+      
+      [_ (error 'simplify "got ~a" s-exp)]))
+  
+  (check-equal? (simplify '(if 1 (letrec ([x (if 2 3)]) 4 5)))
+                '(if 1 (letrec ((x (if 2 3 (void)))) (begin 4 5)) (void))))
 
-(simplify '(if 1 (letrec ([x (if 2 3)]) 4 5)))
+(require (submod "." test))
